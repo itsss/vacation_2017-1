f2454b86e101a3df987b2bc9d3edd65d0f0b1723
Taewon Kang <itschool@itsc.kr>
2017-08-10 22:49:18 +0900

    수행평가 문제풀이 - 삼각화단 만들기 - 2017.08.10 (필요없는 if문 제거)

--- exam_live(2017-1)/exam-02/N.cpp
+++ exam_live(2017-1)/exam-02/N.cpp
@@ -7,13 +7,7 @@ int main()
 	
 	for(a = N / 3 + (N % 3 != 0); a < N / 2 + N % 2; a++)
 	{
-		for(b = (N - a) / 2 + (N - a) % 2; b <= a; b++)
-		{
-			if(a < b + (N - a - b))
-			{
-				cnt++;
-			}
-		}
+		for(b = (N - a) / 2 + (N - a) % 2; b <= a; b++) cnt++;
 	}
 	
 	printf("%d", cnt);
