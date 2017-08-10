60722c356f4480075c7c3f9516679f70c0a8a54f
Taewon Kang <itschool@itsc.kr>
2017-08-10 23:54:34 +0900

    std 함수 사용

---
+++ Programming I/ch02/search algorithm/upper bound_std.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <algorithm>
+
+int n, k, data[1000001];
+
+int main()
+{
+	scanf("%d %d", &n, &k);
+	for(int i = 0; i < n; i++) scanf("%d", data+i);
+	printf("%d\n", std::upper_bound(data, data+n, k) - data + 1 ); 
+	
+	//A[0]~A[n-1]에서 k의 위치 구하기, 
+	//그 주소에서 A를 빼면 k가 존재하는 배열 A의 인덱스, 배열의 인덱스는 0부터 시작, 1 더해주기 
+}
