a1a3098009520e93a381ae506fd7dc50504d8506
Taewon Kang <itschool@itsc.kr>
2017-08-10 17:42:56 +0900

    수행평가 문제풀이 - 삼각화단 만들기(S) - 2017.08.10

---
+++ exam_live(2017-1)/exam-02/N.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int N, cnt = 0, a, b;
+	scanf("%d", &N);
+	
+	for(a = N / 3 + (N % 3 != 0); a < N / 2 + N % 2; a++)
+	{
+		for(b = (N - a) / 2 + (N - a) % 2; b <= a; b++)
+		{
+			if(a < b + (N - a - b))
+			{
+				cnt++;
+			}
+		}
+	}
+	
+	printf("%d", cnt);
+}
